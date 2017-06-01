4157dbf1e9ea3a691ac30e2324243c4048c77e09
Czupryniak, Michal <michal.czupryniak@pearson.com>
2017-06-01 16:09:11 +0200

    Load new data and handle selest event

--- src/app/ng2-smart-table-example/ng2-smart-table-example.component.ts
+++ src/app/ng2-smart-table-example/ng2-smart-table-example.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
 
 @Component({
   selector: 'ng2-smart-table-example',
@@ -53,16 +54,21 @@ export class Ng2SmartTableExampleComponent {
     { name: 'Austin', gender: 'Male', company: 'Swimlane' },
     { name: 'Dany', gender: 'Male', company: 'KFC' },
     { name: 'Molly', gender: 'Female', company: 'Burger King' },
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King' },
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King' },
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
     { name: 'Molly', gender: 'Female', company: 'Burger King' }
   ];
+
+  newData = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' }
+  ];
+
+  source = new LocalDataSource(this.rows);
 
   onDeleteConfirm(event) {
     if (window.confirm('Are you sure you want to delete?')) {
@@ -88,5 +94,14 @@ export class Ng2SmartTableExampleComponent {
     } else {
       event.confirm.reject();
     }
+  }
+
+  rowSelect(event) {
+    console.log("Selected: ", event.data);
+    console.log("Count of element: ", this.source.count());
+  }
+
+  loadData() {
+    this.source.load(this.newData);
   }
 }
