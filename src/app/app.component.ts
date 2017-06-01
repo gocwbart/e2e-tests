2b5ddbcebbd9683b7cf5a5505e2acfcc356f909f
Marek Sklodowski <marek.sklodowski@pearson.com>
2017-06-01 17:48:10 +0200

    Added SCSS and MDL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,10 @@ import { USER_GET } from './store/profile/profile.actions';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  theText: string;
 
   observable$: Observable<{}>;
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MdlModule } from '@angular-mdl/core';
 
 import { AppComponent } from './app.component';
 import { routing } from './app.router';
@@ -18,6 +19,7 @@ import { WeatherService } from './weather/weather.service';
     SharedModule,
     FormsModule,
     HttpModule,
+    MdlModule,
     store,
     effects,
     routing,
