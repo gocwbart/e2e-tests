5789ed1e5338dd2c21e46713d3491cd0512af50f
Roziewicz, Radosław <radoslaw.roziewicz@pearson.com>
2017-06-08 14:37:13 +0200

    common lint file for server and client

--- server/routes/schools.ts
+++ server/routes/schools.ts
@@ -1,9 +1,9 @@
-import { Request, Response, Router } from "express";
+import { Request, Response, Router } from 'express';
 
 const schoolsRouter: Router = Router();
 
 // FIXME: For test purposes only :)
-schoolsRouter.get("/", (request: Request, response: Response) => {
+schoolsRouter.get('/', (request: Request, response: Response) => {
   const limit = +request.query.limit || 10;
   const page = +request.query.page || 1;
   const schools = Array(limit)
@@ -12,7 +12,7 @@ schoolsRouter.get("/", (request: Request, response: Response) => {
       const schoolId = num + index;
 
       return {
-        name: "School " + schoolId,
+        name: 'School ' + schoolId,
         students: Math.pow(schoolId, 3),
         classes: Math.pow(schoolId, 2),
         teachers: schoolId % 8,
